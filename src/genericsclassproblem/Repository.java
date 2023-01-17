56956fcb9e2777819928ab3463ca0d59ea7bd55f
Prashant Deepake <deepakeprashant1@gmail.com>
2023-01-18 00:08:29 +0530

    ADDED : Three String Compare Greater

--- src/genericsclassproblem/Repository.java
+++ src/genericsclassproblem/Repository.java
@@ -4,30 +4,30 @@ import java.util.Scanner;
 
 public class Repository {
     public void getInputByUser() {
-        Float firstNumber, secondNumber, thirdNumber;
+        String firstNumber, secondNumber, thirdNumber;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the first Float number : ");
-        firstNumber = scanner.nextFloat();
+        System.out.print("Enter the first String : ");
+        firstNumber = scanner.next();
 
-        System.out.print("Enter the Second Float number : ");
-        secondNumber = scanner.nextFloat();
+        System.out.print("Enter the Second String : ");
+        secondNumber = scanner.next();
 
-        System.out.print("Enter the Third Float number : ");
-        thirdNumber = scanner.nextFloat();
+        System.out.print("Enter the Third String : ");
+        thirdNumber = scanner.next();
 
         findMaxNumber(firstNumber, secondNumber, thirdNumber);
     }
 
-    private void findMaxNumber(Float firstNumber, Float secondNumber, Float thirdNumber) {
+    private void findMaxNumber(String firstNumber, String secondNumber, String thirdNumber) {
         if ((firstNumber.compareTo(secondNumber) > 0)) {
             if ((firstNumber.compareTo(thirdNumber) > 0)) {
-                System.out.println("First number is grater");
+                System.out.println("First String is grater");
             } else
-                System.out.println("Third number is greater");
+                System.out.println("Third String is greater");
         } else if ((secondNumber.compareTo(thirdNumber)) > 0)
-            System.out.println("Second number is greater");
+            System.out.println("Second String is greater");
         else
-            System.out.println("Third number is greater");
+            System.out.println("Third String is greater");
     }
 }
