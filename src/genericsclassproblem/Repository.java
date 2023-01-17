d87ca3aa1d381a9a6e254d54730987c793a628e1
Prashant Deepake <deepakeprashant1@gmail.com>
2023-01-18 00:00:28 +0530

    ADDED : Find Max No In Float Value

--- src/genericsclassproblem/Repository.java
+++ src/genericsclassproblem/Repository.java
@@ -4,22 +4,22 @@ import java.util.Scanner;
 
 public class Repository {
     public void getInputByUser() {
-        Integer firstNumber, secondNumber, thirdNumber;
+        Float firstNumber, secondNumber, thirdNumber;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the first number : ");
-        firstNumber = scanner.nextInt();
+        System.out.print("Enter the first Float number : ");
+        firstNumber = scanner.nextFloat();
 
-        System.out.print("Enter the Second number : ");
-        secondNumber = scanner.nextInt();
+        System.out.print("Enter the Second Float number : ");
+        secondNumber = scanner.nextFloat();
 
-        System.out.print("Enter the Third number : ");
-        thirdNumber = scanner.nextInt();
+        System.out.print("Enter the Third Float number : ");
+        thirdNumber = scanner.nextFloat();
 
         findMaxNumber(firstNumber, secondNumber, thirdNumber);
     }
 
-    private void findMaxNumber(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
+    private void findMaxNumber(Float firstNumber, Float secondNumber, Float thirdNumber) {
         if ((firstNumber.compareTo(secondNumber) > 0)) {
             if ((firstNumber.compareTo(thirdNumber) > 0)) {
                 System.out.println("First number is grater");
